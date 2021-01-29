06943a6808914270e16404daa030a603956d2416
darrengp <ceo.darrenperry@gmail.com>
2021-01-29 13:50:06 -0800

    after lunch help

--- js/bomb.js
+++ js/bomb.js
@@ -2,12 +2,6 @@
 const intialTime = 30;
 /*----- app's state (variables) -----*/
 
-let blueWire = document.getElementById("blue");
-let greenWire = document.getElementById("green");
-let redWire = document.getElementById("red");
-let whiteWire = document.getElementById("white");
-let yellowWire = document.getElementById("yellow");
-// console.log(blueWire);
 // variable to store interger
 let timeRemaining = 0;
 // to store timers setInterval so we can clear it
@@ -24,13 +18,80 @@ let wireState = {
   yellow: false,
 };
 
+///////////////
+let blueWire = document.getElementById("blue");
+let greenWire = document.getElementById("green");
+let redWire = document.getElementById("red");
+let whiteWire = document.getElementById("white");
+let yellowWire = document.getElementById("yellow");
+console.log(blueWire);
+
+// Array of wires need to be clicked
+
+let wiresToCut = [];
+
 /*----- cached element references -----*/
+
+// event listner on wires /////////////
+let wireboxEl = null;
+// each wire
+let wires = [];
+
+// background img
+let backgroundEl = null;
+
+let timerEl = null;
 
 /*----- event listeners -----*/
 //event listners on wires
 // set interval
 // clear interval
+let resetButtonEL = null;
 
+// set interval timer function
+
+function updateClock() {
+  console.log("coutdown the timer!");
+}
+
+// app intializers sets up game state
+
+function intializeGame() {
+  console.log("let the game begin");
+}
+
+/// handles reset button game
+function resetGame() {
+  console.log("reset game");
+}
+
+// handles rese button click
+function cutWire(event) {
+  console.log("cut a wire", event);
+}
+
+// handles game over state
+function endGame(isGameWon) {
+  console.log("end game");
+}
+
+////////////// eventlistener ////////////
 document.addEventListener("DOMContentLoaded", function () {
   console.log("loaded!");
+  // main element to manipulate the background
+  backgroundEl = document.querySelector("main");
+  // Timer
+  timerEl = document.querySelector("#timer");
+
+  // reset button
+  resetButtonEL = document.querySelector("#resetButton");
+  //   console.log(resetButtonEL);
+  resetButtonEL.addEventListener("click", resetGame);
+  //wirebox
+  wireboxEl = document.querySelector("#wires");
+  wireboxEl.addEventListener("click", cutWire);
+  // each wire elements in an array
+  wires = wireboxEl.children;
+  // init the game
+  intializeGame();
 });
